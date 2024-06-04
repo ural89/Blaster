d2527219de991990f19ad7bc11bd7afe1f0271a0
ural89 <uralkemal@gmail.com>
2024-06-04 20:39:09 +0300

    blaster anim logic

---
+++ Source/Blaster/Private/Character/BlasterAnimInstance.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/BlasterAnimInstance.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "BlasterAnimInstance.h"
+#include "BlasterCharacter.h"
+
+void UBlasterAnimInstance::NativeInitializeAnimation()
+{
+    Super::NativeInitializeAnimation();
+    BlasterCharacter = Cast<ABlasterCharacter>(TryGetPawnOwner());
+}
+
+void UBlasterAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+    Super::NativeUpdateAnimation(DeltaTime);
+
+    if (BlasterCharacter == nullptr)
+    {
+        BlasterCharacter = Cast<ABlasterCharacter>(TryGetPawnOwner());
+    }
+
+    if (BlasterCharacter == nullptr) return;
+
+    FVector Velocity = BlasterCharacter->GetVelocity();
+    Velocity.Z = 0;
+    Speed = Velocity.Size();
+
+    bIsInAir = BlasterCharacter->GetCharacterMovement()->IsFalling();
+
+    bIsAccelerating = BlasterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0;
+
+}
